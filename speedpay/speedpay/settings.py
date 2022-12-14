787fb745b8b031b37b4364d892fecc648fd2d151
Chukwunonso David Ilonze <nonsodavilo@gmail.com>
2022-12-14 08:38:42 +0100

    collected my static files

--- speedpay/speedpay/settings.py
+++ speedpay/speedpay/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-v#%y@fs^9p&uxwt#zio(_p_+b@@9__p3p4xplueegg-00i4ao5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['nonsodavilo.pythonanywhere.com',]
+ALLOWED_HOSTS = ['nonsodavilo.pythonanywhere.com', '127.0.0.1', ]
 
 
 # Application definition
@@ -126,6 +127,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT ='/home/nonsodavilo/Speedpay-banking/speedpay/static'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
