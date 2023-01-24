6769c8a8e59849715e59a72a0108b31f76d6084c
Chukwunonso David Ilonze <nonsodavilo@gmail.com>
2023-01-24 10:02:29 +0100

    added pdf documentation

--- speedpay/api/admin.py
+++ speedpay/api/admin.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
 
-from .models import Customer ,Account, Withdraw, Deposit, AppUser, AppOTP, AppUserToken
+from .models import Customer ,Account, Withdraw, Deposit, Transfer, AppUser, AppOTP, AppUserToken
 
 # Register your models here.
 admin.site.register(Customer)
 admin.site.register(Account)
 admin.site.register(Withdraw)
 admin.site.register(Deposit)
+admin.site.register(Transfer)
 admin.site.register(AppUser)
 admin.site.register(AppOTP)
 admin.site.register(AppUserToken)
